0cc54fdc59c706637c5851da21f266f3b262e08f
Robin Houston <robin.houston@gmail.com>
2018-04-12 18:17:09 +0100

    Eliminate archiver as a suspect

--- src/index.js
+++ src/index.js
@@ -7,31 +7,28 @@ Stripped-down test case for kiln/flourish-sdk#40.
 const fs = require("fs"),
       path = require("path"),
 
-      archiver = require("archiver"),
       request = require("request"),
       tmp = require("tmp");
 
 function makeRequest() {
 	return new Promise(function(resolve, reject) {
-		request.post("https://app.flourish.studio/api/v1/nosuchmethod", resolve);
+		request.post("https://www.google.com", resolve);
 	});
 }
 
-function createEmptyZipFile() {
+function createEmptyFile() {
 	return new Promise(function(resolve, reject) {
 		tmp.file(function(error, zip_filename, zip_fd) {
 			if (error) return reject(error);
 
-			let zip = archiver.create("zip", {});
+			console.log("Creating %s (%d)", zip_filename, zip_fd);
 			let output = fs.createWriteStream(null, { fd: zip_fd });
 			output.on("close", function() {
-				resolve(zip_filename);
+				resolve();
 			});
-
-			zip.pipe(output);
-			zip.finalize();
+			output.close();
 		});
 	});
 }
 
-createEmptyZipFile().then(makeRequest);
+createEmptyFile().then(makeRequest);
