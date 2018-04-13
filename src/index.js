f5d5648bf302e219abf3a21c3b90d75067640ca7
Robin Houston <robin.houston@gmail.com>
2018-04-13 11:12:47 +0100

    Minimal test case

--- src/index.js
+++ src/index.js
@@ -4,28 +4,7 @@
 Stripped-down test case for kiln/flourish-sdk#40.
 */
 
-const fs = require("fs"),
-      https = require("https"),
-      path = require("path"),
-
-      tmp = require("tmp");
-
-function makeRequest() {
-	return new Promise(function(resolve, reject) {
-		https.get("https://www.google.com", resolve);
-	});
-}
-
-function createEmptyFile() {
-	return new Promise(function(resolve, reject) {
-		tmp.file(function(error, filename, fd) {
-			if (error) return reject(error);
-
-			let output = fs.createWriteStream(null, { fd });
-			output.on("close", resolve);
-			output.close();
-		});
-	});
-}
-
-createEmptyFile().then(makeRequest);
+require("tmp").file(function(error, filename, fd) {
+	require("fs").closeSync(fd);
+	require("https").get("https://www.google.com");
+});
